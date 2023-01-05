584c33479c1d57830fde8bd43bece9801cd645f6
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-01-05 16:51:09 +0300

    counter

--- js/challenge.js
+++ js/challenge.js
@@ -6,4 +6,52 @@
 //pause should stop the count
 //submit should display the comment on the dom
 
+let a = 0
+let num =  document.querySelector("h1#counter").innerHTML
 
+let interval = setInterval(function(){
+    num = a
+    a++
+    document.querySelector("h1#counter").innerHTML = a
+  
+   
+}, 1000)
+
+document.querySelector("button#minus").addEventListener("click",function(){
+    document.querySelector("h1#counter").innerHTML -= 1 
+} )
+
+
+document.querySelector("button#plus").addEventListener("click", function(){
+    let string = document.querySelector("h1#counter").innerHTML 
+    str = Number.parseInt(string) + 1 
+    document.querySelector("h1#counter").innerHTML = str
+    
+     
+})
+
+let click = 0
+document.querySelector("button#pause").addEventListener("click", function(e){
+    click = click + 1
+   
+    if(click === 1){
+       
+    clearInterval(interval)}
+
+    else if(click === 2){
+        let interval = setInterval(function(){
+            num = a
+            a++
+            document.querySelector("h1#counter").innerHTML = a
+          
+           
+        }, 1000)
+    }
+    else{
+      
+        clearInterval(interval) 
+        click = click - 1
+        console.log(click)
+    }
+    
+})
