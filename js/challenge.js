93ba03d51e42c082e5ad664140d452fa17ec1669
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-01-05 12:24:27 +0300

    pseudocode

---
+++ js/challenge.js
@@ -0,0 +1,9 @@
+//pseudocode
+//the number should be always increment when domloads upto a set timeout.
+//the minus sign should decrement the count
+//the plus sign should increment the count
+//the love icon once clicked should display a message with the count of how many times the diaplyed number is clicked.
+//pause should stop the count
+//submit should display the comment on the dom
+
+
